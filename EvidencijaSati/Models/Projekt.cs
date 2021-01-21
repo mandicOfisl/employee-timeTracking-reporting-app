3c5c79c26f9748e614506899391e7285d1159b75
Luka Mandić <mandicluka62@gmail.com>
2021-01-21 21:26:46 +0100

    Projekt model

---
+++ EvidencijaSati/Models/Projekt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvidencijaSati.Models
+{
+	 public class Projekt
+	 {
+		  public int Id { get; set; }
+		  public string Naziv { get; set; }
+		  public int KlijentId { get; set; }
+		  public DateTime DatumOtvaranja { get; set; }
+		  public int VoditeljProjektaId { get; set; }
+	 }
+}
