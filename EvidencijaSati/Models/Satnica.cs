0dfaf27d1ddfd3c0f2d61f3143224e87f0fb7267
Luka Mandić <mandicluka62@gmail.com>
2021-01-22 00:56:10 +0100

    Satnica model

---
+++ EvidencijaSati/Models/Satnica.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EvidencijaSati.Models
+{
+	 public class Satnica
+	 {
+		  public int IDSatnica { get; set; }
+		  public DateTime Datum { get; set; }
+		  public Dictionary<Projekt, float> ProjektSati { get; set; }
+		  public string Komentar { get; set; }
+	 }
+}
